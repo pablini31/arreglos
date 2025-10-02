6637bc763e1300a6a42268cf0d5130c377921f21
JesusTxr <chuchito27tm@gmail.com>
2025-10-01 20:55:32 -0600

    ejercicios corregidos listos para compilar

--- arreglos-main/obj/Debug/net8.0-windows/ArrayExercises.WinForms.AssemblyInfo.cs
+++ arreglos-main/obj/Debug/net8.0-windows/ArrayExercises.WinForms.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ArrayExercises.WinForms")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae47105e5fc48cdd029fa4cab72ea4fcf036c6fc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b159c76b55146c74b7d1076b8f0713e4f436a7d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ArrayExercises.WinForms")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ArrayExercises.WinForms")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
